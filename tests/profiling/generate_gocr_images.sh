e6d379c0b36ab2333139925246f58e6de015e6d1
juanjo <juanjo@gti.uvigo.es>
2023-12-06 19:34:19 +0000

    Script to generate GOCR images improved

--- tests/profiling/generate_gocr_images.sh
+++ tests/profiling/generate_gocr_images.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 declare -r directory="images/gocr"
-declare -r font="mono"
-declare -r words=10
+declare -r font="roboto"
+declare -ar words=($(seq 10 1 30))
 declare -ar resolutions=(72 108 144)
 declare -r number_images=100
 
+for number_words in ${words[*]}; do
+
 for i in $(seq $number_images); do
 
-shuf -n "$words" /usr/share/dict/american-english > "${directory}/${font}/${words}/${i}.txt"
+mkdir -p "${directory}/${font}/${number_words}/txt"
+shuf -n "$number_words" /usr/share/dict/american-english > "${directory}/${font}/${number_words}/txt/${i}.txt"
+
 for resolution in ${resolutions[*]}; do
-  pango-view --dpi="$resolution" --font="$font" -qo "${directory}/${font}/${words}/${resolution}dpi/${i}.png" "${directory}/${font}/${words}/${i}.txt"
-  pngtopnm "${directory}/${font}/${words}/${resolution}dpi/${i}.png" > "${directory}/${font}/${words}/${resolution}dpi/${i}.pnm"
+  mkdir -p "${directory}/${font}/${number_words}/${resolution}dpi/png"
+  pango-view --dpi="$resolution" --font="$font" -qo "${directory}/${font}/${number_words}/${resolution}dpi/png/${i}.png" "${directory}/${font}/${number_words}/txt/${i}.txt"
+  mkdir -p "${directory}/${font}/${number_words}/${resolution}dpi/pnm"
+  pngtopnm "${directory}/${font}/${number_words}/${resolution}dpi/png/${i}.png" > "${directory}/${font}/${number_words}/${resolution}dpi/pnm/${i}.pnm"
+done
+
 done
 
 done
